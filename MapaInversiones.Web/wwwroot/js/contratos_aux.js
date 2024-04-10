13775add337c50bdae9eda4d106954d9b6d17788
afvillamizar <villamizarvecino@hotmail.com>
2024-04-09 22:18:49 -0500

    Ajustes
    Ajustes

--- MapaInversiones.Web/wwwroot/js/contratos_aux.js
+++ MapaInversiones.Web/wwwroot/js/contratos_aux.js
@@ -48,13 +48,13 @@
 
 
 Number.prototype.formatMoney = function (c, d, t) {
-    var n = this;
-    var    c = isNaN(c = Math.abs(c)) ? 2 : c;
-    var    d = d == undefined ? "." : d;
-    var    t = t == undefined ? "," : t;
-    var    s = n < 0 ? "-" : "";
-    var    i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "";
-    var    j = (j = i.length) > 3 ? j % 3 : 0;
+    n = this;
+    c = isNaN(c = Math.abs(c)) ? 2 : c;
+    d = d == undefined ? "." : d;
+    t = t == undefined ? "," : t;
+    let s = n < 0 ? "-" : "";
+    let i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "";
+    var j = (j = i.length) > 3 ? j % 3 : 0;
     return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3***REMOVED***)(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
 ***REMOVED***;
 
